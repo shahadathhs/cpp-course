1e3cfb88e25554639eccd2c6616bcda0642a2dee
shahadathhs <shahadathhossensajib732@gmail.com>
2025-09-14 20:22:40 +0600

    chore: check setup

--- main.cpp
+++ main.cpp
@@ -1,12 +1,12 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#include "intro/hello_world.cpp"
+// #include "intro/hello_world.cpp"
 
 int main() {
     cout << "Welcome to the C++ Course" << endl;
 
-    helloWorld();
+    // helloWorld();
 
     return 0;
 }
